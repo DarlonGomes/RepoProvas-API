ecef8ad71c2c7d5e2ecdcf40f7c7cdf5d947bd07
Darlon Gomes <gomesdarlon@icloud.com>
2022-09-14 15:43:23 -0300

    feat: user test

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { userRouter } from "./userRouter";
 
 export const router = Router();
 
+router.use("/user", userRouter);
---
+++ tests/user.test.ts
@@ -0,0 +1,6 @@
+import app from "../src/app"
+import supertest from 'supertest'
+
+describe("POST /users", ()=>{
+
+});
